aa5e509a298325054473c296ec760d1c8bc9ca03
JonathanChiii <chiqingyuan@hotmail.com>
2022-06-18 19:56:18 -0400

    modified ListingsComponent to multiple function component

--- src/components/ListingsComponent.js
+++ src/components/ListingsComponent.js
@@ -1,50 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class Listings extends Component {
-    /*
-    this.state: the array : items are removed from the state, and lifted up to the use of props
-    State is the local state of the component which cannot be accessed and modified outside of the component.
-    It's equivalent to local variables in a function. 
-    Props, on the other hand, make components reusable by giving components the ability to receive data from their parent component in the form of props. 
-    from stackoverflow
-    */
-    /* all state has been moved to maincomponent */
+function RenderListingItem({ item, onClick }) {
+    return (
+        //<Card onClick={() => this.props.onClick(item.id)}> removed this.props.
+        <Card onClick={() => onClick(item.id)}>
+            <CardImg width="100%" src={item.image} alt={item.name} />
+            <CardImgOverlay>
+                <CardTitle>{item.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
+/*
+this.state: the array : items are removed from the state, and lifted up to the use of props
+State is the local state of the component which cannot be accessed and modified outside of the component.
+It's equivalent to local variables in a function. 
+Props, on the other hand, make components reusable by giving components the ability to receive data from their parent component in the form of props. 
+from stackoverflow
+*/
+/* all state has been moved to maincomponent */
 
 
-    /* this component doesn't maintain any local state, and purely work on the props that passed to it. */
+/* this component doesn't maintain any local state, and purely work on the props that passed to it. */
 
-
-
-    /* added to demo the life cycle methods of the component */
-    componentDidMount() {
-        console.log("Listings component did mount");
-    }
-
-    render() {
-        console.log("Listings component did rendered");
-
-        const listings = this.props.items.map((item) => {
-            return (
-                <div key={item.id} className="col-5 col-md-4 col-lg-2 m-1">
-                    <Card onClick={() => this.props.onClick(item.id)}>
-                        <CardImg width="100%" src={item.image} alt={item.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{item.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
-
+const Listings = (props) => {
+    //     const listings = this.props.items.map((item) => ...); removed this.
+    const listings = props.items.map((item) => {
         return (
-            <div className="container">
-                <div className="row">
-                    {listings}
-                </div>
+            <div key={item.id} className="col-5 col-md-4 col-lg-2 m-1">
+                <RenderListingItem item={item} onClick={props.onClick} />
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {listings}
+            </div>
+        </div>
+    );
 }
 
 export default Listings;
