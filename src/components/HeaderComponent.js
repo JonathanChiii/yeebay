226b7bc78faa213ce6126011706e0dc0e5f0bebd
JonathanChiii <chiqingyuan@hotmail.com>
2022-06-20 16:36:40 -0400

    added react routers, navlink, NavbarToggler, collape

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,19 +1,68 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
+class Header extends Component {
 
-class Header extends Component{
-    render(){
-        return(
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isNavOpen: false
+        }
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav = () => {
+        this.setState((prevState) => (
+            {
+                isNavOpen: !prevState.isNavOpen
+            }
+        )
+        );
+
+        /*
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        })
+        */
+
+        //console.log('isNavOpen is' + this.state.isNavOpen);
+    }
+
+    render() {
+        return (
             //use react fragment, or <React.Fragment> without adding an extra node into the DOM
             <>
-                <Navbar dark>
+                <Navbar dark expand='md'>
                     <div className='container'>
-                        <NavbarBrand href="http://google.com"> YeeBay </NavbarBrand>
+
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" href="http://google.com">
+                            <img src="/assets/images/logo.PNG" height='50' width='140' alt='YeeBay' />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className='nav-link' to='./myaccount'>
+                                        <span className="fa fa-home fa-lg"></span> My Account
+                                    </NavLink>
+                                    <NavLink className='nav-link' to='./home'>
+                                        <span className="fa fa-home fa-lg"></span> Home
+                                    </NavLink>
+                                    <NavLink className='nav-link' to='./listings'>
+                                        <span className="fa fa-list fa-lg"></span> Listings
+                                    </NavLink>
+                                    <NavLink className='nav-link' to='./aboutus'>
+                                        <span className="fa fa-info fa-lg"></span> About Us
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
 
-                
+
                 <div className='container'>
                     <div className="row row-header">
                         <div className="col-12 col-sm-6">
@@ -22,7 +71,6 @@ class Header extends Component{
                         </div>
                     </div>
                 </div>
-                
             </>
         )
     }
